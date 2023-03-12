bb0a339833eb1bf8c3c7d88b9657aef081acaea9
Manuel Rodriguez <manuel.rodriguez15@libero.it>
2023-03-13 00:32:02 +0100

    creating module Leading Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,19 @@
-import React, { useEffect, useState } from 'react'; 
-import './App.css';
-import { landingPageDTO } from './films/films.model'; 
-import spider_man_path from './photos/Spider_Man_Far_from_Home.jpg';
-import moana_path from './photos/moana.jpg';
-import avatar_path from './photos/avatar.jpg';
-import soul_path from './photos/soul.jpg'; 
-import ListFilms from './films/listFilms'; 
+import './App.css'; 
 import Menu from './utils/Menu';
 import { Route, Switch } from 'react-router';
 import GenreIndices from './genres/genreIndices';
 import { BrowserRouter } from 'react-router-dom';
+import LandingPage from './LandingPage';
  
-function App() {
-
-  const [films, setFilms] = useState<landingPageDTO>({})
-
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      setFilms({onTheBillboard: [
-        {
-          id: 1,
-          title: 'Spider-Man: Far from Home',
-          poster: spider_man_path
-        },
-        {
-          id: 2,
-          title: 'Moana',
-          poster: moana_path
-        },
-        {
-          id: 3,
-          title: 'Avatar',
-          poster: avatar_path
-        }
-      ], upcomingFilmReleases: [
-        {
-          id: 4,
-          title: 'Soul',
-          poster: soul_path
-        }
-      ]})
-    }, 500);
-
-    return () => clearTimeout(timerId);
-  })
- 
+function App() { 
   return (
     <> 
     <BrowserRouter>
-      <Menu/>
-  
+      <Menu/> 
       <div className='container'> 
         <Switch>
           <Route exact path="/">
-            <h3>Films On The Billboard</h3>
-            <ListFilms films={films.onTheBillboard} />
-
-            <h3>Upcoming film releases</h3>
-            <ListFilms films={films.upcomingFilmReleases} />
+            <LandingPage />
           </Route>
           <Route path="/genres">
             <GenreIndices />
