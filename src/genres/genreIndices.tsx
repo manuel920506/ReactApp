ce26f501680ad43c7910b0b8cafb68415866baa4
Manuel Rodriguez <manuel.rodriguez15@libero.it>
2023-03-19 02:10:38 +0100

    first comunication between frontend and backend

--- src/genres/genreIndices.tsx
+++ src/genres/genreIndices.tsx
@@ -1,5 +1,16 @@
+import axios , {AxiosResponse} from "axios";
+import { useEffect } from "react";
 import { Link } from "react-router-dom"; 
+import { urlGenres } from "../utils/endpoints";
+import { genreDTO } from "./genres.model";
 export default function GenreIndices(){ 
+    useEffect(() => {
+        axios.get(urlGenres)
+        .then((response: AxiosResponse<genreDTO[]>) => {
+            console.log(response.data);
+        })
+
+    }, [])
     return (
         <>
             <h3>Genre Indices</h3>   
